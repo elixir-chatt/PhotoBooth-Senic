f8da0c86167c59c255177da043ab1af29c035769
Bruce Tate <bruce@rapidred.com>
2019-04-11 19:08:39 -0400

    tweak naming and flesh out state machine functions

--- lib/core/photo_booth.ex
+++ lib/core/photo_booth.ex
@@ -1,14 +1,14 @@
 defmodule Pex.Core.PhotoBooth do
-  defstruct [:mode, :photo, :troll, :count_down, :taken, :photos, :chosen]
-  @seconds_to_countdown 3
+  defstruct [:mode, :photo, :troll, :seconds_to_countdown, :taken, :photos, :chosen]
+  @max_seconds_to_countdown 3
   @pictures_to_take 5
   
   def new do
     %{
-      mode: :ready, #:ready, :countdown, :shooting, :choose, :transmit
+      mode: :ready, #:ready, :countdown, :shooting, :choosing, :transmitting
       photo: <<>>,
       troll: false,
-      count_down: @seconds_to_countdown,
+      seconds_to_countdown: @max_seconds_to_countdown,
       taken: 0,
       photos: [],
       chosen: [],
@@ -20,39 +20,60 @@ defmodule Pex.Core.PhotoBooth do
     %{booth | photo: photo}
   end
   
-  def change_mode(booth, mode) do
-    %{booth | mode: mode }
+  def start(%{mode: :ready}=booth) do
+    %{ booth |
+                seconds_to_countdown: @max_seconds_to_countdown,
+                taken: 0,
+                photos: [],
+                chosen: [],
+    }
+    |> change_mode(:countdown)
   end
   
-  def countdown(%{count_down: 0} = booth) do
+  
+  def countdown(%{seconds_to_countdown: 0} = booth) do
     booth
     |> change_mode(:shooting)
-    |> Map.put(:count_down, @seconds_to_countdown)
+    |> Map.put(:seconds_to_countdown, @max_seconds_to_countdown)
   end
   
   def countdown(booth) do
-    %{booth | count_down: booth.count_down-1 }  
+    %{booth | seconds_to_countdown: booth.seconds_to_countdown-1 }  
   end
   
   def take_picture(booth, photo) do
     booth
     |> Map.put(:photos, [photo | booth.photos])
     |> Map.put(:taken, booth.taken + 1)
-    |> advance
+    |> advance_from_shooting
   end
   
-  def advance(%{taken: @pictures_to_take}=booth) do
+  def advance_from_shooting(%{taken: @pictures_to_take}=booth) do
     booth
     |> Map.put(:taken, 0)
-    |> change_mode(:choose)
+    |> change_mode(:choosing)
   end
-
-  def advance(booth) do
+  def advance_from_shooting(booth) do
     booth
     |> change_mode(:countdown)
   end
   
+  def choose(booth, choices) do
+    booth
+    |> Map.put(:chosen, choices)
+    |> transmit
+    |> change_mode(:transmitting)
+  end
   
+  def change_mode(booth, mode) do
+    %{booth | mode: mode }
+  end
   
+  def transmit(booth), do: booth
+  
+  def finish(%{mode: :transmitting}=booth) do
+    change_mode(booth, :ready)
+  end
+  def finish(booth), do: booth
   
 end
