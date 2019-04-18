35ed19c60235b990e013932d767e13ca2ad06faf
Grace McPherson <grace.theprogrammer@gmail.com>
2019-04-18 14:17:29 -0500

    cleaned tests for readability using our assert_key function

--- test/core_test.exs
+++ test/core_test.exs
@@ -15,54 +15,74 @@ defmodule CoreTest do
   end
 
   test "test defaults" do
-    assert default().troll == false
-    assert default().seconds_to_countdown == 3
+    default()
+    |> assert_key(:troll, false)
+    |> assert_key(:seconds_to_countdown, 3)
   end
 
   test "test the changing of mode" do
-    booth = default()
+    default()
     |> PhotoBooth.change_mode(:countdown)
-    assert booth.mode == :countdown
+    |> assert_key(:mode, :countdown)
   end
 
   test "test countdown" do
-    booth = default()
+    default()
     |> PhotoBooth.countdown()
     |> assert_key(:seconds_to_countdown, 2)
     |> PhotoBooth.countdown()
+    |> assert_key(:seconds_to_countdown, 1)
     |> PhotoBooth.countdown()
     |> assert_key(:seconds_to_countdown, 0)
   end
 
   test "test countdown full" do
-    booth = default()
+    default()
     |> PhotoBooth.countdown()
     |> PhotoBooth.countdown()
     |> PhotoBooth.countdown()
+    |> assert_key(:seconds_to_countdown, 0)
     |> PhotoBooth.countdown()
-    assert booth.seconds_to_countdown == 3
-    assert booth.mode == :shooting
+    |> assert_key(:seconds_to_countdown, 3)
+    |> assert_key(:mode, :shooting)
   end
 
   test "test take pictures" do
-    booth = default()
+    default()
     |> PhotoBooth.take_picture(<<>>)
     |> PhotoBooth.take_picture(<<>>)
     |> PhotoBooth.take_picture(<<>>)
-    assert booth.photos == [<<>>, <<>>, <<>>]
-    assert booth.taken == 3
+    |> assert_key(:photos, [<<>>, <<>>, <<>>])
+    |> assert_key(:taken, 3)
   end
 
   test "test advancing the mode back to countdown" do
-    booth = default()
+    default()
     |> PhotoBooth.change_mode(:shooting)
     |> PhotoBooth.advance_from_shooting()
-    assert booth.mode == :countdown
+    |> assert_key(:mode, :countdown)
   end
 
   test "test advancing the mode to the selection process" do
-    booth = full()
-    assert booth.mode == :choosing
+    full()
+    |> assert_key(:mode, :choosing)
+  end
+
+  test "test start choosing mode" do
+    full()
+    |> PhotoBooth.choose([1,2,3])
+    |> assert_key(:mode, :transmitting)
+  end
+
+  test "test proper finish" do
+    full()
+    |> PhotoBooth.choose([1,2,3])
+    |> PhotoBooth.finish
+    |> PhotoBooth.start
+    |> assert_key(:seconds_to_countdown, 3)
+    |> assert_key(:taken, 0)
+    |> assert_key(:photos, [])
+    |> assert_key(:chosen, [])
   end
 
   test "test functions do not fire unless in appropriate state" do
@@ -71,31 +91,12 @@ defmodule CoreTest do
     assert PhotoBooth.finish(booth) == booth
   end
 
-  test "test start choosing mode" do
-    booth = full()
-    choices = [1,2,3]
-    assert PhotoBooth.choose(booth,choices).mode == :transmitting
-  end
-  
-  test "test proper finish" do
-    booth = full()
-    choices = [1,2,3]
-    
-    expected = 
-      PhotoBooth.choose(booth,choices)
-      |> PhotoBooth.finish
-      |> PhotoBooth.start
 
-    assert expected.seconds_to_countdown == 3
-    assert expected.taken == 0
-    assert expected.photos == []
-    assert expected.chosen == []
-  end
-  
   defp assert_key(booth, key, actual) do
     expected = Map.get(booth, key)
-      
+
     assert expected == actual
     booth
   end
+
 end
